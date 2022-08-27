e4ab691f51af1b034c4e55f92127195148e16d84
Anshu Kumar <anshu.study20@gmail.com>
2022-08-28 01:32:55 +0530

    1 st one

---
+++ ArraysCC.java
@@ -0,0 +1,9 @@
+public class ArraysCC{
+    public static void main(String[] args) {
+        int marks[] = new int[50]; // size - 50
+        int numbers[] = {1, 2, 3}; //size -3
+        String fruits[] = {"apple", "mango", "orange"}; //size-3
+
+    }
+    
+}
